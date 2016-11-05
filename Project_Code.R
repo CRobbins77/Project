7bb231c4a84bb114f6073a94fdd0b812741ac933
CRobbins77 <curtisrobbins@msn.com>
2016-11-05 16:50:58 -0400

    Code intro

---
+++ Project_Code.R
@@ -0,0 +1,10 @@
+#Load raw data for wholesale distribution
+wholesale <- read.csv("ny_county_wholesale.csv", header = TRUE)
+
+#Examine the imported dataset
+head(wholesale)
+summary(wholesale)
+
+
+
+
